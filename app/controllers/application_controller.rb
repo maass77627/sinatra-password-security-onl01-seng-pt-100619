5cbadabac57449741b3399db55b3c9b2cdc6eefe
kelly r maass <kmaass77627@gmail.com>
2020-02-09 06:39:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ end
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		 
+
 		  if user && user.authenticate(params[:password])
 		    session[:user_id] = user.id
 		    redirect "/success"
