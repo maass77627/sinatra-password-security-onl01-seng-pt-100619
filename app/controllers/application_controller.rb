0837a6768c60378293c524172b693887c0152cb9
kelly r maass <kmaass77627@gmail.com>
2020-02-09 07:08:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,11 +61,11 @@ end
 
 	helpers do
 		def logged_in?
-			!!session[:id]
+			!!session[:user_id]
 		end
 
 		def current_user
-			User.find(session[:id])
+			User.find(session[:user_id])
 		end
 	end
 
